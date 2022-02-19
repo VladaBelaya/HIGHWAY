6a8a63ae5a3e1f2c00a78199667be6c0fac29eb0
VladaBelaya <vlada_chvanova@mail.ru>
2022-02-19 15:01:39 +0300

    added delivery progress status

--- app/js/app.js
+++ app/js/app.js
@@ -1,24 +1,10 @@
 const	swiper = new Swiper(".swiper", {
-		slidesPerView: 6,
+		slidesPerView: 5,
 		spaceBetween: 30,
 		centeredSlides: false,
 		pagination: {
 			el: ".swiper-pagination",
 			clickable: true,
 		},
-	breakpoints: {
-		640: {
-			slidesPerView: 2,
-			spaceBetween: 20,
-		},
-		768: {
-			slidesPerView: 4,
-			spaceBetween: 20,
-		},
-		1024: {
-			slidesPerView: 5,
-			spaceBetween: 20,
-		},
-	},
 	});
 
